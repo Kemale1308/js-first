a64be3cb9688c79c1499c607aa03bb36ca1fee2c
Kemale1308 <esgerovak@gmail.com>
2024-08-13 08:54:20 +0400

    first

---
+++ app.js
@@ -0,0 +1,11 @@
+// Sehifeni  yeniləyəndə 95 ilə 100 arası təsadüfi bir rəqəm çap etsin uslu uslu.
+
+
+console.log(Math.floor(Math.random() * 6) + 95);
+//ilk once bilirik ki math.random 0 ve 1 arasi random ededler verir eger bunu 6 ya vurub math.floor a salsaq 
+//0 ve 5 arasi reqemler alaciq niye bes 6 cunki max 0,9 *6 5 tam necese edir onuda floor elesek 5 qalir 
+//demeli bu yolla 0 dan 5 e qeder 5 daxil random ededler elde edecik sonra o randomun ustune 95 gelende 
+//Mellimin sertine gore 95 ile 100 arasi random eded elde edecik.
+
+
+// ./ qoymaqi unutma 
